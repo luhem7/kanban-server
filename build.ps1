67f8ec9b692832413a8468be876af66ca9b27aa0
Mehul Gangavelli <mehul.gang@gmail.com>
2015-09-25 17:39:37 -0400

    Added more options to the build script

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 Param(
-    [Parameter(Mandatory=$False)][boolean]$onlyCopyRes=$False
+    [Parameter(Mandatory=$False)][boolean]$skipCopyRes=$False,
+    [Parameter(Mandatory=$False)][boolean]$skipCompileGo=$False
 )
 
 #Script variables
@@ -8,11 +9,17 @@ $src_dir = $Env:GOPATH + "\src\" + $project_name
 $bin_dir = $Env:GOPATH + "\bin\" + $project_name
 $res_path = "\res"
 
-Write-Host "Building "$project_name" project"
-Write-Host "Copying Res folder to bin directory"
-Copy-Item ($src_dir+$res_path) $bin_dir -recurse -force
+If($skipCopyRes){
+    Write-Host "Skipping copying Res folder to bin directory"
+} Else {
+    Write-Host "Building "$project_name" project"
+    Write-Host "Copying Res folder to bin directory"
+    Copy-Item ($src_dir+$res_path) $bin_dir -recurse -force
+}
 
-If(-Not($onlyCopyRes)) {
+If($skipCompileGo) {
+    Write-Host "Skipping building the go executable"
+} Else {
     Write-Host "Building the go executable"
     go build $project_name
 }
