8511082b9c905a06d709e1ef69c337bb44489582
Mehul Gangavelli <mehul.gang@gmail.com>
2015-09-26 06:20:52 -0400

    Added force clean of res directory to build script

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 Param(
     [Parameter(Mandatory=$False)][boolean]$skipCopyRes=$False,
-    [Parameter(Mandatory=$False)][boolean]$skipCompileGo=$False
+    [Parameter(Mandatory=$False)][boolean]$skipCompileGo=$False,
+    [Parameter(Mandatory=$False)][boolean]$skipForceClean=$False
 )
 
 #Script variables
@@ -9,19 +10,26 @@ $src_dir = $Env:GOPATH + "\src\" + $project_name
 $bin_dir = $Env:GOPATH + "\bin\" + $project_name
 $res_path = "\res"
 
+If($skipCompileGo) {
+    Write-Host "Skipping building the go executable"
+} Else {
+    Write-Host "Building the go executable"
+    Set-Location $bin_dir
+    go build $project_name
+}
+
+If($skipForceClean){
+    Write-Host "Skipping force clean of res directory"
+} Else {
+    Remove-Item ($bin_dir+$res_path) -recurse -force
+}
+
 If($skipCopyRes){
     Write-Host "Skipping copying Res folder to bin directory"
 } Else {
     Write-Host "Building "$project_name" project"
     Write-Host "Copying Res folder to bin directory"
     Copy-Item ($src_dir+$res_path) $bin_dir -recurse -force
-}
-
-If($skipCompileGo) {
-    Write-Host "Skipping building the go executable"
-} Else {
-    Write-Host "Building the go executable"
-    go build $project_name
 }
 
 Write-Host "Done"
